6de187c879fe354f7667726f323608515bccae6b
Rui Tong <torry_tong@outlook.com>
2024-01-24 17:53:12 -0800

    rt - added service to get information on joke query

--- src/main/java/edu/ucsb/cs156/spring/backenddemo/services/JokeQueryService.java
+++ src/main/java/edu/ucsb/cs156/spring/backenddemo/services/JokeQueryService.java
@@ -1,13 +1,22 @@
 package edu.ucsb.cs156.spring.backenddemo.services;
 
-import lombok.extern.slf4j.Slf4j;
-
+import java.util.List;
 import java.util.Map;
 
+import lombok.extern.slf4j.Slf4j;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.springframework.web.client.RestTemplate;
+
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.RestTemplate;
 
 
 @Slf4j
@@ -24,8 +33,16 @@ public class JokeQueryService {
 
     public String getJSON(String category, String numJokes) throws HttpClientErrorException {
         log.info("category={}, numJokes={}", category, numJokes);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(List.of(MediaType.APPLICATION_JSON));
+        headers.setContentType(MediaType.APPLICATION_JSON);
 
         Map<String, String> uriVariables = Map.of("category", category, "numJokes", numJokes);
-        return "";
+
+        HttpEntity<String> entity = new HttpEntity<>("body", headers);
+
+        ResponseEntity<String> re = restTemplate.exchange(ENDPOINT, HttpMethod.GET, entity, String.class,uriVariables);
+
+        return re.getBody();
     }
 }
