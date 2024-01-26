2123a5a773da6d4476bbc8d28230baa1cc80f072
Megumi Ondo <megumiondo@169-231-147-90.wireless.ucsb.edu>
2024-01-25 16:18:53 -0800

    mo - added service to get information on univrrsity

--- src/main/java/edu/ucsb/cs156/spring/backenddemo/services/UniversityQueryService.java
+++ src/main/java/edu/ucsb/cs156/spring/backenddemo/services/UniversityQueryService.java
@@ -13,6 +13,21 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 
+
+import java.util.List;
+import java.util.Map;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+
 @Slf4j
 @Service
 public class UniversityQueryService {
@@ -25,6 +40,9 @@ public class UniversityQueryService {
     }
 
     public static final String ENDPOINT = "http://universities.hipolabs.com/search?name={name}";
+
+    //     public static final String ENDPOINT = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&minmagnitude={minMag}&maxradiuskm={distance}&latitude={latitude}&longitude={longitude}";
+
 
     public String getJSON(String name) throws HttpClientErrorException {
        return "";
